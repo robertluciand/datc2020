c19f5e4cc3b420c52d69f857725ee254c0f85618
robertluciand <65246056+robertluciand@users.noreply.github.com>
2020-10-04 22:37:41 +0300

    Update Student.cs

--- Duicu Robert Lucian/L02/Student.cs
+++ Duicu Robert Lucian/L02/Student.cs
@@ -5,7 +5,7 @@ namespace L02
     {
         public int id{ get; set;}
         public string nume {get;set;}
-        public string facultate{get;set;}//facultate
+        public string facultate{get;set;}
         public int an{get;set;}
         public Student(int i, string n, string f, int a){
             id=i;
